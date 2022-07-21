67c24bb92cd4900a683829bcd6fdf320aa35a9eb
Manupatino16 <manuelap1604@gmail.com>
2022-07-21 17:43:07 -0500

    se modifica el index y se crea controlador

---
+++ src/controllers/controlador.js
@@ -0,0 +1,11 @@
+//CREANDO VARIABLES
+
+let nombreUsuario="Juan Jose"
+let EDAD_USUARIO=33
+let estaturaUsuario=1.62
+let hinchaDelVerde=true
+
+console.log(nombreUsuario)
+
+console.log(EDAD_USUARIO)
+
