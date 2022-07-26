5cdb276b8ceeb6daea247c9ab803535be74019a7
Manupatino16 <manuelap1604@gmail.com>
2022-07-26 17:35:12 -0500

    version3

---
+++ src/controllers/aprendiendoDOM.js
@@ -0,0 +1,11 @@
+//PASOS PARA CONTROLAR UNA ETIQUETA DESDE JS
+
+//1. CONTROLANDO EL CONTENIDO
+let etiquetaTitulo=document.getElementById("titulo")
+let etiquetaImagen=document.getElementById("foto")
+
+//1.1 MANIPULANDO EL TEXTO DE LA ETIQUETA(ELEMENTO)
+etiquetaTitulo.textContent="jefe usted es como mi padre"
+
+//1.2 MANIPULANDO EL SRC
+etiquetaImagen.src="https://www.cultture.com/pics/2021/07/la-policia-fue-llamada-a-la-casa-de-rihanna-en-los-angeles-tras-recibir-informes-de-un-intruso.jpg"
---
+++ src/controllers/baseDatos.js
@@ -0,0 +1,19 @@
+//QUEMAR (HARD CORE) LA BD
+let productos=[
+
+    {
+        nombre:"ESENCIAS",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurampb.appspot.com/o/producto1.webp?alt=media&token=b1460dfc-a88e-4d27-86bb-93b6952e9895","https://firebasestorage.googleapis.com/v0/b/tiendasurampb.appspot.com/o/producto1.1.webp?alt=media&token=60176eb4-8072-45bd-8f9d-d20607b3065b"],
+        precio:13786,
+        popularidad:0,
+        descripcion:" Despierte el apetito y gusto de sus clientes al utilizar nuestras esencias. Se emplea para caracterizar los sabores en la elaboración de panes, tortas, pasteles, galletas y postres. Este es un productos elaborado en forma de emulsion, resultando un līquido denso totalmente soluble en cualquier tipo de masa o batido. No desaparece durante el horneo!"
+    },
+    {
+        nombre:"",
+        fotos:[],
+        precio:0,
+        popularidad:0,
+        descripcion:"" 
+    }
+
+]
