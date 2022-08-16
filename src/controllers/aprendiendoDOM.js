529674406fd64693259134032f8d6f0403214f62
407 <407@academicos.cesde.edu.co>
2022-08-16 17:40:30 -0500

    continuacion

--- src/controllers/aprendiendoDOM.js
+++ src/controllers/aprendiendoDOM.js
@@ -9,3 +9,15 @@ etiquetaTitulo.textContent="jefe usted es como mi padre"
 
 //1.2 MANIPULANDO EL SRC
 etiquetaImagen.src="https://www.cultture.com/pics/2021/07/la-policia-fue-llamada-a-la-casa-de-rihanna-en-los-angeles-tras-recibir-informes-de-un-intruso.jpg"
+
+//2. CONTROLANDO EL DISEÑO
+let parrafo=document.getElementById("parrafo")
+parrafo.textContent="mmmmmmmmmmmmmmmmmmmmmmmmm"
+
+//2.1 AGREGANDO UN ESTILO (CLASS)
+//text-danger
+parrafo.classList.add("text-danger","fs-1","text-center")
+etiquetaImagen.classList.add("d-lock","mx-auto")
+
+//2.2 QUITAR UN ESTILO ()
+parrafo.classList.remove("text-danger")
